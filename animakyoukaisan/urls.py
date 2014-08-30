1b9c2e35825460d7b39377db9991207a10547eae
Nap Joseph Calub <njncalub@gmail.com>
2014-08-31 01:44:27 +0800

    updated urls

--- animakyoukaisan/urls.py
+++ animakyoukaisan/urls.py
@@ -2,9 +2,11 @@ from django.conf import settings
 from django.conf.urls import patterns, include, url
 from django.conf.urls.static import static
 from django.contrib import admin
+from django.http import HttpResponseRedirect
 
 admin.autodiscover()
 
 urlpatterns = patterns('',
     url(r'^admin/', include(admin.site.urls)),
+    url(r'^$', lambda x: HttpResponseRedirect('/admin/')),
 ) + static(settings.MEDIA_URL, document_root=settings.MEDIA_ROOT)
