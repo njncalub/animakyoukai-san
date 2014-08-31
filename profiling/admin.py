7f986979dca22a1a4da6b50b34cdf38f408c0c19
Nap Joseph Calub <njncalub@gmail.com>
2014-08-31 10:51:47 +0800

    added admin filters

--- profiling/admin.py
+++ profiling/admin.py
@@ -4,5 +4,6 @@ from profiling.models import Person
 class PersonAdmin(admin.ModelAdmin):
     model = Person
     list_display = ('image_tag', 'last_name', 'first_name', 'nick_name', 'sex', 'birth_date', 'mobile_number', 'member_status')
+    list_filter = ('sex', 'application_date', 'member_status')
 
 admin.site.register(Person, PersonAdmin)
