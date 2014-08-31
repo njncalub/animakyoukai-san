c546a18018f62fa38f1660d8b0d764d9f6f6664a
Nap Joseph Calub <njncalub@gmail.com>
2014-08-31 12:17:53 +0800

    added search filter

--- profiling/admin.py
+++ profiling/admin.py
@@ -5,5 +5,6 @@ class PersonAdmin(admin.ModelAdmin):
     model = Person
     list_display = ('image_tag', 'last_name', 'first_name', 'nick_name', 'sex', 'birth_date', 'mobile_number', 'member_status')
     list_filter = ('sex', 'application_date', 'member_status')
+    search_fields = ['last_name', 'first_name', 'middle_name', 'nick_name', 'mobile_number', 'landline_number', 'address']
 
 admin.site.register(Person, PersonAdmin)
